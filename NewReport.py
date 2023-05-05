93dd0bb9970d3855214da99ed0f315661973587a
Neeraj5690 <Neeraj@408>
2023-05-05 14:06:35 -0400

    New changes

--- NewReport.py
+++ NewReport.py
@@ -4,7 +4,7 @@ import datetime
 import pandas as pd
 
 version = " version: 1.0 "
-ExcelFileName = "ReportData"
+ExcelFileName = "ReportData_Neeraj"
 
 Report_Title="Report"
 Project_Name = "QA"
@@ -181,7 +181,7 @@ class PDF(FPDF):
         self.set_text_color(80, 80, 80)
         self.cell(0, 1, version +"    Report Date: "+ctReportHeader, 0, 0, 'L')
         try:
-            self.image('ModuleVsBugsCount.jpg', 100, 25, 100,90)
+            self.image('C:/Users/Neeraj Kumar/.jenkins/workspace/Create_Graph/Neeraj_ModuleVsBugsCount.jpg', 100, 25, 100,90)
         except:
             self.image('img.jpg', 100, 25, 100, 90)
 
