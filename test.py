127e056965dd33b25257495078de72f8c77d0977
Neeraj5690 <Neeraj@408>
2023-05-08 17:03:15 -0400

    New changes

--- test.py
+++ test.py
@@ -1,5 +1,20 @@
-import os
-from pathlib import Path
-home = str(Path.home())
-home = home.replace(os.sep, '/')
-print(home)
+from github import Github
+
+g = Github("Neeraj5690", "Neeraj@408")
+g = Github("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")
+
+#  All repos present
+for repo in g.get_user().get_repos():
+    print(repo.name)
+
+try:
+    # Accessing particular Repo and its folder
+    repo=g.get_repo("Neeraj5690/Reporting")
+    # Removing files from the folder
+    Folder=repo.get_contents("/ReportData")
+    for contentFiles in Folder:
+        print(contentFiles)
+
+        repo.delete_file(contentFiles.path, "message", contentFiles.sha, branch='master')
+except:
+    print("No File present")
